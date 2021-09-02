7a0598a78fb97c8f91169139b73c26cd5cd1b961
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-09-02 20:27:27 +0530

    [Mayur] Refactored : minor icons css style change.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService
+{
+
+  private sendingSource = new BehaviorSubject([]);
+  receivingSource = this.sendingSource.asObservable();
 
   constructor() { }
 }
